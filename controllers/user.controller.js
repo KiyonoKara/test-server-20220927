f21da69732c435b7a7ab4083489ad66eaa280bd0
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2023-10-11 20:42:46 -0400

    Change response render for users

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,6 +3,7 @@ import UserAccessor from "../db_accessor/user.accessor.js";
 export default class UserController {
     static async getAllUsers(req, res) {
         const users = await UserAccessor.getAllUsers();
-        res.json(users);
+        console.log(users);
+        res.render("index", { users: users });
     }
 }
