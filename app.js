0a048c72d8343b75c0b7d84be04fad2fa1bb0886
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2023-10-11 20:41:41 -0400

    Phase 4: Use EJS

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ import route from "./routes/pages.route.js";
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
 app.use("/", route);
-app.use("/page2", route);
+// app.use("/page2", route);
 
 export default app;
