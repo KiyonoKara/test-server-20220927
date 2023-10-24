b04b90ed3fe577857f8879838f02516ac814f17b
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2023-10-24 00:27:25 -0400

    Features: Add more pages and features such as a login page with a working sign-in, following other users, and logging out

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,113 @@
 import UserAccessor from "../db_accessor/user.accessor.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import Auth from "../auth/authorization.js";
 
 export default class UserController {
     static async getAllUsers(req, res) {
         const users = await UserAccessor.getAllUsers();
-        console.log(users);
-        res.render("index", { users: users });
+        res.render("index", {users: users});
     }
-}
+
+    static async getLoginPage(req, res) {
+        if (req.cookies.token) {
+            res.redirect("/profile");
+        } else {
+            res.render("login_page", {error: req.cookies.error});
+        }
+    }
+
+    static async getSignUpPage(req, res) {
+        if (req.cookies.token) {
+            res.redirect("/profile");
+        } else {
+            res.render("sign_up");
+        }
+    }
+
+    static async postSignUp(req, res, next) {
+        try {
+            req.body.password = await bcrypt.hash(req.body.password, 10);
+            await UserAccessor.createUser(req.body);
+            res.redirect("/login-page");
+        } catch (e) {
+            req.error = 999;
+            next();
+        }
+    }
+
+    static async postLogin(req, res, next) {
+        try {
+            if (!req.cookies.token) {
+                const user = await UserAccessor.getUser(req.body.username);
+                if (user) {
+                    const result = await bcrypt.compare(req.body.password, user.password);
+                    if (result) {
+                        const token = jwt.sign(
+                            {
+                                username: user.username,
+                                email: user.email,
+                                bio: user.bio,
+                                followers: user.followers,
+                                following: user.following
+                            },
+                            process.env.TOKEN_KEY
+                        );
+                        res.cookie("token", token, {
+                            httpOnly: true,
+                            maxAge: 60 * 60 * 1000
+                        });
+                        res.redirect("/profile");
+                    } else {
+                        req.error = 400;
+                        next();
+                    }
+                } else {
+                    req.error = 400;
+                    next();
+                }
+            } else {
+                res.redirect("/profile");
+            }
+        } catch (e) {
+            req.error = 400;
+            next();
+        }
+    };
+
+    static async getProfile(req, res, next) {
+        if (!req.error) {
+            const user = Auth.getUserInfo(req);
+            res.render("profile", {
+                name: user.username,
+                email: user.email,
+                bio: user.bio,
+                followers: user.followers,
+                following: user.following
+            });
+        } else {
+            return next();
+        }
+    }
+
+    static async getLogout(req, res) {
+        res.clearCookie("token");
+        res.redirect("/");
+    }
+
+    static async followUser(req, res, next) {
+        if (!req.error) {
+            const toFollow = req.body.follow;
+            const user = Auth.getUserInfo(req);
+            const username = user.username;
+            const following = user.following;
+
+            if (following.every((follower) => { follower !== toFollow; }) && toFollow !== username) {
+                await UserAccessor.addFollower(username, toFollow);
+            }
+            res.redirect("/");
+        } else {
+            return next();
+        }
+    };
+};
--- db_accessor/user.accessor.js
+++ db_accessor/user.accessor.js
@@ -2,6 +2,16 @@ import Connection from "../db/connection.js";
 import User from "../models/user.js";
 
 export default class UserAccessor {
+    static async getUser(username) {
+        try {
+            await Connection.open("users");
+            const user = await User.findOne({ username: username });
+            return user;
+        } catch (e) {
+            throw e;
+        }
+    }
+
     static async getAllUsers() {
         try {
             await Connection.open("users");
@@ -9,7 +19,37 @@ export default class UserAccessor {
             for await (const doc of User.find()) {
                 users.push(doc);
             }
+            console.log(users);
             return users;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    static async createUser(userDoc) {
+        try {
+            await Connection.open("users");
+            const user = await User.create(userDoc);
+            return user;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    static async addFollower(userWhoFollowed, userToFollow) {
+        try {
+            await Connection.open("users");
+            const follower = await UserAccessor.getUser(userWhoFollowed);
+            const followee = await UserAccessor.getUser(userToFollow);
+
+            const followerList = follower.following;
+            followerList.push(userToFollow);
+
+            const followeeList = followee.followers;
+            followeeList.push(userWhoFollowed);
+
+            await User.findOneAndUpdate({ username: userWhoFollowed }, { following: followerList });
+            await User.findOneAndUpdate({ username: userToFollow }, { following: followeeList });
         } catch (e) {
             throw e;
         }
